580c621de746b3ea811a8b210b3eeb61b0ddadb1
Jessica <jessicamorrison.webdev@gmail.com>
2021-06-02 10:45:11 -0700

    created cart reducer

---
+++ frontend/src/reducers/cartReducers.js
@@ -0,0 +1,28 @@
+import { CART_ADD_ITEM } from "../constants/cartConstants";
+
+export const cartReducer = (state = { cartItems: [] }, action) => {
+  switch (action.type) {
+    case CART_ADD_ITEM:
+      const item = action.payload;
+      console.log("In cart reducer--" + item);
+      const existItem = state.cartItems.find((x) => x.product === item.product);
+      console.log("Console log of existItem ---" + existItem);
+
+      if (existItem) {
+        return {
+          ...state,
+          cartItems: state.cartItems.map((x) =>
+            x.product === existItem.product ? item : x
+          ),
+        };
+      } else {
+        return {
+          ...state,
+          cartItems: [...state.cartItems, item],
+        };
+      }
+
+    default:
+      return state;
+  }
+};
