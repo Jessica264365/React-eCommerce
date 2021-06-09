f147e84d2bc3a8045b9a87da2fb7d59aacad8dd9
Jessica <jessicamorrison.webdev@gmail.com>
2021-06-09 10:33:55 -0700

    Created cart reducer and add to cart action

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -4,9 +4,9 @@ export const cartReducer = (state = { cartItems: [] }, action) => {
   switch (action.type) {
     case CART_ADD_ITEM:
       const item = action.payload;
-      console.log("In cart reducer--" + item);
+      
       const existItem = state.cartItems.find((x) => x.product === item.product);
-      console.log("Console log of existItem ---" + existItem);
+     
 
       if (existItem) {
         return {
--- frontend/src/screens/cartScreen.js
+++ frontend/src/screens/cartScreen.js
@@ -1,6 +1,36 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  Row,
+  Col,
+  Image,
+  ListGroup,
+  Form,
+  Card,
+  Button,
+} from "react-bootstrap";
+import Message from "../components/message";
+import { addToCart } from "../actions/cartActions";
 
-const CartScreen = () => {
+// Get location.search to get the query string for the qty
+
+const CartScreen = ({ match, location, history }) => {
+  // get product id from URL
+  const productId = match.params.id;
+
+  // location.search = "?qty=i". I need the quantity only so the string is split at "=" and I get the value of the index of 1
+  // qty won't be a number datatype so the Number constructor is used
+  const qty = location.search ? Number(location.search.split("=")[1]) : 1;
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (productId) {
+      dispatch(addToCart(productId, qty));
+    }
+  }, [dispatch, productId, qty]);
+
   return <div>Cart</div>;
 };
 
